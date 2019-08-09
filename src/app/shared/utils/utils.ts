7c92ee01dea038d75e741607233216322c178c4c
Morgan Thompson <morgan.thompson@gov.bc.ca>
2019-08-09 14:03:26 -0700

    refactor filename encoding into shared utils
      * cleanup old encoder logic, use shared utils encoder
      * remove old encode method from api

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -42,4 +42,22 @@ export class Utils {
 
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
+
+  public encode(component: string) {
+        let encoded = encodeURIComponent(component);
+        return encoded.replace(/[!'()*]/g, (c) => {
+        // Also encode !, ', (, ), and *
+          return '%' + c.charCodeAt(0).toString(16);
+        });
+  }
+
+  public encodeFilename(filename: string, isUrl: boolean) {
+    let safeName;
+    if (isUrl) {
+        return safeName = this.encode(filename).replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\\/g, '_').replace(/\//g, '_').replace(/\%2F/g, '_');
+    } else {
+        return safeName = filename.replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\\/g, '_').replace(/\//g, '_');
+    }
+
+  }
 }
