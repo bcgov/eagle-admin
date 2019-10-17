5a2bcd17fd064ecb34109244c511c832e2ecbd3f
Alastair Beaumont <a.beaumont11@gmail.com>
2019-10-17 15:17:06 -0700

    Hot reloading effort
    Still hitting the keycloak auth step which is causing a full page reload still on every save

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,20 @@ import 'hammerjs'; // https://material.angular.io/guide/getting-started#step-5-g
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'rxjs/add/operator/filter';
+import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
