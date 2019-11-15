ab5deb2828fa2fa47c6940374f6365cd7dd8855a
Alastair Beaumont <a.beaumont11@gmail.com>
2019-11-15 15:34:07 -0800

    Contact saving (#319)
    * Adding in the contact logic into our new tabs
    
    * adding in the override to the 2018 tab
    
    * Updating project details to include the contact info

--- src/app/project/project-detail/project-detail.component.ts
+++ src/app/project/project-detail/project-detail.component.ts
@@ -18,7 +18,7 @@ import { StorageService } from 'app/services/storage.service';
 import { SearchService } from 'app/services/search.service';
 import { ISearchResults } from 'app/models/search';
 import { Utils } from 'app/shared/utils/utils';
-
+import { flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-detail',
@@ -57,6 +57,8 @@ export class ProjectDetailComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.route.parent.data
       .takeUntil(this.ngUnsubscribe)
+      // Mapping the get People object observable here to fill out the epd and lead objects
+      .flatMap(data => this.projectService.getPeopleObjs(data))
       .subscribe(
         (data: { project: ISearchResults<Project>[] }) => {
           if (data.project) {
@@ -91,7 +93,8 @@ export class ProjectDetailComponent implements OnInit, OnDestroy {
     this.storageService.state.project = this.project;
     this.storageService.state.tableColumns = null;
     this.storageService.state.sortBy = null;
-    this.storageService.state.form = null;
+    this.storageService.state.form2002 = null;
+    this.storageService.state.form2018 = null;
     this.storageService.state.selectedContactType = null;
     this.storageService.state.componentModel = null;
     this.storageService.state.rowComponent = null;
