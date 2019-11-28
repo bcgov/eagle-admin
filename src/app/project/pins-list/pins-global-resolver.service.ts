4b04c2c2b917f24d1d399a1cf624272d76ec94c3
Morgan Thompson <morgan.thompson@gov.bc.ca>
2019-11-28 14:55:06 -0800

    remove duplicate sidebar option, filter organizations by group

--- src/app/project/pins-list/pins-global-resolver.service.ts
+++ src/app/project/pins-list/pins-global-resolver.service.ts
@@ -29,7 +29,7 @@ export class PinsGlobalComponentResolver implements Resolve<Observable<object>> 
       sortBy,
       {},
       false,
-      {},
+      { companyType: 'Indigenous Group' },
       '',
     );
   }
