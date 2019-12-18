2e568bdacfd2ea096f2cbd233de6c80197d4e53c
Baraka Lwakila <baraka.lwakila@gov.bc.ca>
2019-12-17 16:10:34 -0800

    multi select working for pins

--- src/app/shared/components/link-organization/link-organization-table-rows/link-organization-table-rows.component.ts
+++ src/app/shared/components/link-organization/link-organization-table-rows/link-organization-table-rows.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter, OnDestroy } from '@angular/core';
 
 import { TableComponent } from 'app/shared/components/table-template/table.component';
 import { TableObject } from 'app/shared/components/table-template/table-object';
 import { Router } from '@angular/router';
 import { StorageService } from 'app/services/storage.service';
 import { NavigationStackUtils } from 'app/shared/utils/navigation-stack-utils';
+import { Org } from 'app/models/org';
 
 @Component({
   selector: 'tbody[app-link-organization-table-rows]',
@@ -18,6 +19,7 @@ export class LinkOrganizationTableRowsComponent implements OnInit, TableComponen
 
   public organizations: any;
   public paginationData: any;
+  public showCheckboxes = false;
 
   constructor(
     private router: Router,
@@ -27,10 +29,33 @@ export class LinkOrganizationTableRowsComponent implements OnInit, TableComponen
 
   ngOnInit() {
     this.organizations = this.data.data;
+    this.showCheckboxes = this.storageService.state.showOrgTableCheckboxes;
     this.paginationData = this.data.paginationData;
+    this.organizations.forEach((org: Org) => {
+      org.checkbox = this.storageService.state.selectedOrgs.some(element => {
+        return org._id === element._id;
+      });
+    });
   }
 
-  selectItem(item) {
+  selectItem(item: Org): void {
+    item.checkbox = !item.checkbox;
+    if (this.storageService.state.selectedOrgs || this.storageService.state.selectedOrgs === []) {
+      if (item.checkbox) {
+        this.storageService.state.selectedOrgs.push(item);
+      } else {
+        this.storageService.state.selectedOrgs = this.storageService.state.selectedOrgs.filter(function (value, index, arr) {
+          return value._id !== item._id;
+        });
+      }
+    }
+  }
+
+  onRowClick(item: Org): void {
+    this.showCheckboxes ? this.selectItem(item) : this.saveSingleItem(item);
+  }
+
+  saveSingleItem(item: Org): void {
     this.storageService.state.selectedOrganization = item;
 
     if (this.storageService.state.add) {
@@ -40,9 +65,7 @@ export class LinkOrganizationTableRowsComponent implements OnInit, TableComponen
       this.storageService.state.selectedOrganization = null;
       this.storageService.state.add = null;
     }
-
-    let url = this.navigationStackUtils.getLastBackUrl();
-    this.navigationStackUtils.popNavigationStack();
-    this.router.navigate(url);
   }
+
+
 }
