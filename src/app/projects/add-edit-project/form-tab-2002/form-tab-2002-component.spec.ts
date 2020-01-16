7e3b7e03a4403f8135c54545abf884e1a686eddb
Alastair Beaumont <a.beaumont11@gmail.com>
2020-01-16 15:25:18 -0800

    more work on the unit tests

--- src/app/projects/add-edit-project/form-tab-2002/form-tab-2002-component.spec.ts
+++ src/app/projects/add-edit-project/form-tab-2002/form-tab-2002-component.spec.ts
@@ -1,17 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormTab2002Component } from './form-tab-2002.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('FormTab2002', () => {
   let component: FormTab2002Component;
   let fixture: ComponentFixture<FormTab2002Component>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [FormTab2002Component]
+      declarations: [FormTab2002Component],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
       .compileComponents();
   }));
   beforeEach(() => {
     fixture = TestBed.createComponent(FormTab2002Component);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
   });
 
