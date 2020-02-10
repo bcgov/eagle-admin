9a4d16fa79aedd7d344806b3604d0abb935ce57f
Alastair Beaumont <a.beaumont11@gmail.com>
2020-02-10 11:53:41 -0800

    Loading the lists from the config service
    making sure those are loaded before the document page loads. So the objectids can be converted

--- src/app/project/project-documents/detail/document-detail-resolver.service.ts
+++ src/app/project/project-documents/detail/document-detail-resolver.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
-import { SearchService } from 'app/services/search.service';
 import { DocumentService } from 'app/services/document.service';
+import { ConfigService } from 'app/services/config.service';
 
 @Injectable()
 export class DocumentDetailResolver implements Resolve<Observable<object>> {
   constructor(
-    private documentService: DocumentService
+    private documentService: DocumentService,
+    private configService: ConfigService
   ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<object> {
     const docId = route.paramMap.get('docId');
-    return this.documentService.getById(docId);
+    // We need to make sure that getLists populates the configService. Then just returns the documentService getById
+    return this.configService.getLists()
+   .switchMap(_ => this.documentService.getById(docId));
   }
 }
