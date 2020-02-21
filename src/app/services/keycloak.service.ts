64586b140a2af87f3e01bb2d4c1c2dba8a7e07f1
Unknown <juliansubda@gmail.com>
2020-02-21 12:27:00 -0800

    Porting over CAP changes

--- src/app/services/keycloak.service.ts
+++ src/app/services/keycloak.service.ts
@@ -63,11 +63,14 @@ export class KeycloakService {
     if (this.keycloakEnabled) {
       // Bootup KC
       this.keycloakEnabled = true;
+
+      const keycloak_client_id = window.localStorage.getItem('from_admin_server--keycloak_clientid_env');
+
       return new Promise((resolve, reject) => {
         const config = {
           url: this.keycloakUrl,
           realm: this.keycloakRealm,
-          clientId: 'eagle-admin-console'
+          clientId: (_.isEmpty(keycloak_client_id)) ? 'eagle-admin-console' : keycloak_client_id
         };
 
         // console.log('KC Auth init.');
