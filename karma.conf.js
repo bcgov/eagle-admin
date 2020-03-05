acdc57234ba28b6b9662f1612dc0f437703cbf96
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-03-05 08:47:31 -0800

    Upping Jasmine timeout

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,10 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        timeoutInterval: 10000
+      }
     },
     files: [
 
