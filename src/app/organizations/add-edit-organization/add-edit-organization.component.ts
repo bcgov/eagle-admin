253ced7b7a453e1902455368e52fe1f831c76225
a1astair <alastairbeaumont@protonmail.com>
2020-03-17 14:32:46 -0700

    using the get last back url because the popnavigationstack is null

--- src/app/organizations/add-edit-organization/add-edit-organization.component.ts
+++ src/app/organizations/add-edit-organization/add-edit-organization.component.ts
@@ -215,7 +215,7 @@ export class AddEditOrganizationComponent implements OnInit, OnDestroy {
       this.orgService.add(org)
         .subscribe(item => {
           if (this.navigationStackUtils.getLastBackUrl()) {
-            this.router.navigate(this.navigationStackUtils.popNavigationStack().backUrl);
+            this.router.navigate(this.navigationStackUtils.getLastBackUrl());
           } else {
             this.router.navigate(['orgs']);
           }
