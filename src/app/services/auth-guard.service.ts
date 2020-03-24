382b2707ee892e5a81818948207f35e72cf965af
CEBergin <cody.bergin@hotmail.com>
2020-03-24 14:42:28 -0700

    Added workaround for local development to skip authentication to other pages. Bug not present on Dev,Test or Prod.

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { KeycloakService } from 'app/services/keycloak.service';
+import { ApiService } from 'app/services/api';
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private _api: ApiService;
 
   constructor( private router: Router,
-    private keycloakService: KeycloakService, ) {
+    private keycloakService: KeycloakService,
+    private api: ApiService, ) {
+      this._api = api;
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.api.env === 'local'){
+      return true;
+    }
     if (this.keycloakService.isValidForSite()) {
       return true;
     }
